0182f5683efbd997cb4102515c7b69402e082fe6
masoud7777 <46895686+masoud7777@users.noreply.github.com>
2026-06-18 00:10:34 +0330

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,18 @@
+"""
+ماژول مدل‌های هوش مصنوعی
+"""
 
+from .base import BaseModelClient, ModelInfo
+from .ollama_client import OllamaClient
+from .gemini_client import GeminiClient
+from .openai_client import OpenAIClient
+from .anthropic_client import AnthropicClient
+
+__all__ = [
+    "BaseModelClient",
+    "ModelInfo",
+    "OllamaClient",
+    "GeminiClient",
+    "OpenAIClient",
+    "AnthropicClient",
+]
